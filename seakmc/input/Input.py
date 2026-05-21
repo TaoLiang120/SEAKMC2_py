14cc9bb60150d133bc97033dd89d31f6b6e120a2
Tao Liang <xhtliang120@gmail.com>
2026-05-20 20:37:19 -0400

    Modify WS

--- seakmc/input/Input.py
+++ seakmc/input/Input.py
@@ -880,6 +880,11 @@ class Settings:
                 logstr += "\n" + f"This is corresponding to an energy barrier of {barr} eV at {temp} K with a prefactor of {prefactor}!."
                 print(logstr)
 
+        if self.dynamic_matrix["OutDynMat"]:
+            if not self.dynamic_matrix["CalPrefactor"]:
+                errormsg = f"To output dynamic matrix, 'CalPrefactor' has to be True!"
+                raise ValueError(errormsg)
+
         if self.spsearch["insituGuidedSPS"]:
             self.active_volume["Overlapping"] = False
             self.active_volume["Stack4noOverlap"] = False
